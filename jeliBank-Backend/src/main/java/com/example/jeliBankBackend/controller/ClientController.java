6d7208d1c4b6ba8e02c76ca42aa98cf6e85c37dc
lizz medina <lizzmedina15@gmail.com>
2023-10-14 14:16:01 -0500

    completado service y controller de cliente, con CRUD basico

--- jeliBank-Backend/src/main/java/com/example/jeliBankBackend/controller/ClientController.java
+++ jeliBank-Backend/src/main/java/com/example/jeliBankBackend/controller/ClientController.java
@@ -1,4 +1,57 @@
 package com.example.jeliBankBackend.controller;
 
+import com.example.jeliBankBackend.exceptions.ResourseNotFoundException;
+import com.example.jeliBankBackend.model.Client;
+import com.example.jeliBankBackend.service.ClientService;
+import org.hibernate.annotations.UpdateTimestamp;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("api/v1")
 public class ClientController {
+    private ClientService clientService;
+
+    @Autowired
+    public ClientController(ClientService clientService) {
+        this.clientService = clientService;
+    }
+
+    @PostMapping("/client")
+    public Client crateClient(Client client){
+        return this.clientService.createClient(client);
+    }
+
+    @GetMapping("/clients")
+    public List<Client> getAllClients() {
+        return this.clientService.getAllClients();
+    }
+
+    @GetMapping("/client/{idClient}")
+    public Optional<Client> getClientById(@PathVariable Long idClient){
+        return this.clientService.getClientById(idClient);
+    }
+    @GetMapping("/client/{numberDocumentId}")
+    public Optional<Client>  getClientByDocument(Long numberDocumentId){
+        return Optional.ofNullable(this.clientService.getClientByDocument(numberDocumentId));
+    }
+
+    @PutMapping("/client/{clientNumberId}")
+    public ResponseEntity<?> upDateAcount(@RequestBody Client clientNumberId) throws ResourseNotFoundException {
+        clientService.upDateClient(clientNumberId);
+        return ResponseEntity.ok(HttpStatus.OK);
+    }
+
+    @DeleteMapping("/acount/{clientNumberId}")
+    public ResponseEntity<?> deleteClient(@PathVariable Long clientNumberId) throws ResourseNotFoundException{
+        clientService.deleteClient(clientNumberId);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+
 }
--- jeliBank-Backend/src/main/java/com/example/jeliBankBackend/service/ClientService.java
+++ jeliBank-Backend/src/main/java/com/example/jeliBankBackend/service/ClientService.java
@@ -1,5 +1,6 @@
 package com.example.jeliBankBackend.service;
 
+import com.example.jeliBankBackend.exceptions.ResourseNotFoundException;
 import com.example.jeliBankBackend.model.Acount;
 import com.example.jeliBankBackend.model.Client;
 import com.example.jeliBankBackend.repository.ClientRepository;
@@ -7,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Objects;
 import java.util.Optional;
 
 @Service
@@ -27,8 +29,8 @@ public class ClientService {
         return clientRepository.findAll();
     }
 
-    public Optional<Client> getClientById(Long id){
-        return clientRepository.findById(id);
+    public Optional<Client> getClientById(Long idClient){
+        return clientRepository.findById(idClient);
     }
 
     public Client getClientByDocument(Long numberDocumentId){
@@ -40,5 +42,35 @@ public class ClientService {
             return clientOptional.get();
         }
         throw new RuntimeException("No hay ningun cliente para el número ingresado");
+    }
+
+    public Client upDateClient(Client clientToUpdate) throws ResourseNotFoundException {
+
+        Optional<Client> client = clientRepository.findById(clientToUpdate.getNumberDocumentId());
+
+        if (clientToUpdate != null || client.isEmpty()){
+            client.get().setName(Objects.isNull(clientToUpdate.getName()) ?
+                    client.get().getName() : clientToUpdate.getName());
+
+            client.get().setLastName(Objects.isNull(clientToUpdate.getLastName()) ?
+                    client.get().getLastName() : clientToUpdate.getLastName());
+
+            client.get().setNumberDocumentId(Objects.isNull(clientToUpdate.getNumberDocumentId()) ?
+                    client.get().getNumberDocumentId() : clientToUpdate.getNumberDocumentId());
+
+            clientRepository.save(client.get());
+        }else  {
+            throw new ResourseNotFoundException("No existe o no fue posible actualizar la cuenta ingresada");
+        }
+        return clientRepository.save(clientToUpdate);
+    }
+
+    public String deleteClient(Long clientNumberId) throws ResourseNotFoundException {
+        if (clientRepository.findById(clientNumberId).isPresent()){
+            clientRepository.deleteById(clientNumberId);
+            return "Cliente eliminado exitosamente";
+        }else  {
+            throw new ResourseNotFoundException("No existe o no fue posible eliminar el cliente, por favor revise los datos ingresados e intente nuevamnete");
+        }
     }
 }
